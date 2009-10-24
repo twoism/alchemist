5562baf020adaa8e0dbb5b2cbff58e88ae6c87c5
Christopher Burnett <chris.burnett@digitalscientists.com>
2009-10-24 19:09:55 -0400

    added wacky ruby hashing trick

--- tests/alchemist_test.rb
+++ tests/alchemist_test.rb
@@ -2,22 +2,25 @@ require 'rubygems'
 require 'test/unit'
 require 'shoulda'
 require '../lib/alchemist'
+
 class AlchemistTest < Test::Unit::TestCase
+  
   context "Testing Alchemist Distance Units" do
     @distance_table = Alchemist::conversion_table[:distance] 
 
     setup do
-      @conversions = {"meters-miles"=> 0.000621371192237334 }
+      @conversions = {[:meters, :miles]=> 0.000621371192237334 }
     end
     
     @distance_table.keys.each do |from|
       @distance_table.keys.each do |to|
         unless from == to
           should "convert #{from} to #{to}" do  
-            assert_in_delta 1.send(from).to.send(to).to_f, @conversions["#{from.to_s}-#{to.to_s}"], 0.0000001
+            assert_in_delta 1.send(from).to.send(to).to_f, @conversions[[from,to]], 0.0000001
           end 
         end
       end
     end
   end
+  
 end
