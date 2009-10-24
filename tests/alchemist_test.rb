18cfdaea99ff4d59d216f101a33208b8f35738c4
Christopher Burnett <chris.burnett@digitalscientists.com>
2009-10-24 19:56:19 -0400

    adding new tests with yml power

--- tests/alchemist_test.rb
+++ tests/alchemist_test.rb
@@ -1,22 +1,30 @@
 require 'rubygems'
 require 'test/unit'
 require 'shoulda'
+require 'yaml'
+
 require File.join(File.dirname(__FILE__), '..', 'lib', 'alchemist')
+
+
+CONVERSIONS = YAML::load_file('conversions.yml')
+@distance_conversions = CONVERSIONS["distance"].map {|k,v|{ k.split(" to ") => v } }
+
+puts @distance_conversions.inspect
 
 class AlchemistTest < Test::Unit::TestCase
   
   context "Testing Alchemist Distance Units" do
     @distance_table = Alchemist::conversion_table[:distance] 
-
+    
     setup do
-      @conversions = {[:meters, :miles]=> 0.000621371192237334 }
+      @distance_conversions = CONVERSIONS["distance"].map {|k,v|{ k.split(" to ") => v } }
     end
     
     @distance_table.keys.each do |from|
       @distance_table.keys.each do |to|
         unless from == to
           should "convert #{from} to #{to}" do  
-            assert_in_delta 1.send(from).to.send(to).to_f, @conversions[[from,to]], 0.0000001
+            assert_in_delta 1.send(from).to.send(to).to_f, @distance_conversions[[from.to_s,to.to_s]], 0.0000001
           end 
         end
       end
