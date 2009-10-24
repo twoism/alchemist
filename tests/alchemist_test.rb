0f45942a46753941c8bdda65aee136a0c495b9d6
Christopher Burnett <chris.burnett@digitalscientists.com>
2009-10-24 19:02:25 -0400

    whoops

--- tests/alchemist_test.rb
+++ tests/alchemist_test.rb
@@ -3,9 +3,9 @@ require 'test/unit'
 require 'shoulda'
 require '../lib/alchemist'
 class AlchemistTest < Test::Unit::TestCase
-  @conversions = {"meters-miles"=> 0.000621371192237334 }
   context "Testing Alchemist Distance Units" do
     @distance_table = Alchemist::conversion_table[:distance] 
+
     setup do
       @conversions = {"meters-miles"=> 0.000621371192237334 }
     end
@@ -14,15 +14,10 @@ class AlchemistTest < Test::Unit::TestCase
       @distance_table.keys.each do |to|
         unless from == to
           should "convert #{from} to #{to}" do  
-  
             assert_in_delta 1.send(from).to.send(to).to_f, @conversions["#{from.to_s}-#{to.to_s}"], 0.0000001
-  
           end 
         end
       end
     end
-  
   end
-  
-  
 end
